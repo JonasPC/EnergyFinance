b3a36f00224397cdd45790f2e129ddb2332a73b6
JakartaLaw <jeppesjohansen@gmail.com>
2018-04-26 14:42:10 +0100

    done with gdp module

--- src/gdp.py
+++ src/gdp.py
@@ -9,4 +9,37 @@ from src.utils import Utils
 
 class GDP():
 
-    PATH = 'datafolder//raw//longlat//'
+    @staticmethod
+    def load_gdp(phase):
+        """
+        Parameters:
+        ===========
+        phase (string) : {'late', 'early'}
+
+        Returns
+        =======
+        pd.DataFrame (object)
+        """
+
+        if phase is 'early':
+            raw = pd.read_csv('datafolder//raw//gdp//gdp_early_period.csv', skiprows=4)
+        elif phase is 'late':
+            raw = pd.read_csv('datafolder//raw//gdp//gdp_late_period.csv', skiprows=4)
+        else:
+            raise ValueError('Phase must be either "late" or "early"')
+
+        raw.rename(columns={'Area': 'time'}, inplace=True)
+        raw.set_index('time', inplace=True)
+        raw.drop('Fips', axis=1, inplace=True)
+        raw = raw.transpose()
+        raw.dropna(axis=1, inplace=True)
+
+        raw = Utils.rename(raw)
+        return Utils.drop_cols(raw)
+
+    @classmethod
+    def clean_gdp(cls):
+
+        e_gdp = cls.load_gdp('early')
+        l_gdp = cls.load_gdp('late')
+        return pd.concat([e_gdp, l_gdp])
