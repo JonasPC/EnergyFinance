b64943a1f0b0470d0eb67df8f6d21b47c9426e22
JakartaLaw <jeppesjohansen@gmail.com>
2018-05-01 19:33:59 +0100

    created test in Utils

--- src/dataretrieve.py
+++ src/dataretrieve.py
@@ -1,17 +1,15 @@
 import pandas as pd
 
 # own packagees
-from gdp import GDP
-from population import Population
-from longlat import LongLat  # longitude Latitude
-from weather import Weather
-from moments import Moments
-#from prices import Prices
-#from sales import Sales
+from src.utils import Utils
 
 
 class DataRetrieve(object):
 
-    # panel data
+    states = Utils.read_states()
 
+
+s
+
+    pd.concat()
     # cross sectional data
--- src/utils.py
+++ src/utils.py
@@ -1,60 +1,62 @@
 
 class Utils(object):
 
-    @staticmethod
-    def rename(df):
-        df.rename(columns={
-            'AK': 'Alaska',
-            'AL': 'Alabama',
-            'AR': 'Arkansas',
-            'AZ': 'Arizona',
-            'CA': 'California',
-            'CO': 'Colorado',
-            'CT': 'Connecticut',
-            'DC': 'District of Columbia',
-            'DE': 'Delaware',
-            'FL': 'Florida',
-            'GA': 'Georgia',
-            'HI': 'Hawaii',
-            'IA': 'Iowa',
-            'ID': 'Idaho',
-            'IL': 'Illinois',
-            'IN': 'Indiana',
-            'KS': 'Kansas',
-            'KY': 'Kentucky',
-            'LA': 'Louisiana',
-            'ME': 'Maine',
-            'MD': 'Maryland',
-            'MA': 'Massachusetts',
-            'MI': 'Michigan',
-            'MN': 'Minnesota',
-            'MS': 'Mississippi',
-            'MO': 'Missouri',
-            'MT': 'Montana',
-            'NE': 'Nebraska',
-            'NV': 'Nevada',
-            'NH': 'New Hampshire',
-            'NJ': 'New Jersey',
-            'NM': 'New Mexico',
-            'NY': 'New York',
-            'NC': 'North Carolina',
-            'ND': 'North Dakota',
-            'OH': 'Ohio',
-            'OK': 'Oklahoma',
-            'OR': 'Oregon',
-            'PA': 'Pennsylvania',
-            'RI': 'Rhode Island',
-            'SC': 'South Carolina',
-            'SD': 'South Dakota',
-            'TN': 'Tennessee',
-            'TX': 'Texas',
-            'UT': 'Utah',
-            'VT': 'Vermont',
-            'VA': 'Virginia',
-            'WA': 'Washington',
-            'WV': 'West Virginia',
-            'WI': 'Wisconsin',
-            'WY': 'Wyoming'}, inplace=True)
+    states_dict = {
+        'AK': 'Alaska',
+        'AL': 'Alabama',
+        'AR': 'Arkansas',
+        'AZ': 'Arizona',
+        'CA': 'California',
+        'CO': 'Colorado',
+        'CT': 'Connecticut',
+        'DC': 'District of Columbia',
+        'DE': 'Delaware',
+        'FL': 'Florida',
+        'GA': 'Georgia',
+        'HI': 'Hawaii',
+        'IA': 'Iowa',
+        'ID': 'Idaho',
+        'IL': 'Illinois',
+        'IN': 'Indiana',
+        'KS': 'Kansas',
+        'KY': 'Kentucky',
+        'LA': 'Louisiana',
+        'ME': 'Maine',
+        'MD': 'Maryland',
+        'MA': 'Massachusetts',
+        'MI': 'Michigan',
+        'MN': 'Minnesota',
+        'MS': 'Mississippi',
+        'MO': 'Missouri',
+        'MT': 'Montana',
+        'NE': 'Nebraska',
+        'NV': 'Nevada',
+        'NH': 'New Hampshire',
+        'NJ': 'New Jersey',
+        'NM': 'New Mexico',
+        'NY': 'New York',
+        'NC': 'North Carolina',
+        'ND': 'North Dakota',
+        'OH': 'Ohio',
+        'OK': 'Oklahoma',
+        'OR': 'Oregon',
+        'PA': 'Pennsylvania',
+        'RI': 'Rhode Island',
+        'SC': 'South Carolina',
+        'SD': 'South Dakota',
+        'TN': 'Tennessee',
+        'TX': 'Texas',
+        'UT': 'Utah',
+        'VT': 'Vermont',
+        'VA': 'Virginia',
+        'WA': 'Washington',
+        'WV': 'West Virginia',
+        'WI': 'Wisconsin',
+        'WY': 'Wyoming'}
+
+    @classmethod
+    def rename(cls, df):
+        df.rename(columns=cls.states_dict, inplace=True)
 
         return df
 
@@ -70,3 +72,25 @@ class Utils(object):
 
         states = cls.read_states()
         return df[states]
+
+    @classmethod
+    def test_states(cls):
+        """
+        Parameters
+        ==========
+        None
+
+
+        Returns
+        =======
+        set_state1 : (set) states from dict
+        set_state2 : (set) states from columns.txt
+
+
+        """
+        state_list = [cls.states_dict[key] for key in cls.states_dict]
+
+        set_state1 = set(state_list)
+        set_state2 = set(cls.read_states())
+
+        return set_state1, set_state2
